dc5c0c28279bea15a4e742f5100235e83f77cb83
Kubilay Şener <kblysnr2050@gmail.com>
2018-07-21 17:50:40 +0300

    21.07.2018 17.50

--- src/com/kuber/Main.java
+++ src/com/kuber/Main.java
@@ -5,12 +5,19 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Bu proje Berna ve Kubilay tarafından geliştirilmektedir.");
 
-        int myIntVal = 5;
-        double myDoubleVal = 5d;
-        float myFloatVal=5f;
+        //Lecture_1
+
+        int myIntVal = 5 / 3 ;
+        double myDoubleVal = 5d / 3;
+        float myFloatVal=5f/ 3 ;
 
         System.out.println("Integer Value = " + myIntVal);
         System.out.println("Double Value = " + myDoubleVal);
         System.out.println("Float Value = " + myFloatVal);
+
+        // End of Lecture_1
+
     }
 }
+
+
