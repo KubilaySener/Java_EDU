111e73faaea7f104a534a9e9284a19c0ffa1ecf1
Kubilay Şener <kblysnr2050@gmail.com>
2018-07-21 17:21:15 +0300

    Commit.

--- src/com/kuber/Main.java
+++ src/com/kuber/Main.java
@@ -4,5 +4,13 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Bu proje Berna ve Kubilay tarafından geliştirilmektedir.");
+
+        int myIntVal = 5;
+        double myDoubleVal = 5d;
+        float myFloatVal=5f;
+
+        System.out.println("Integer Value = " + myIntVal);
+        System.out.println("Double Value = " + myDoubleVal);
+        System.out.println("Float Value = " + myFloatVal);
     }
 }
