d174e54e9a1688ec5633eea2cc11869d76a0efe8
Kubilay Şener <kblysnr2050@gmail.com>
2018-07-21 17:15:38 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# ProjektBernA
---
+++ src/com/kuber/Main.java
@@ -0,0 +1,8 @@
+package com.kuber;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Bu proje Berna ve Kubilay tarafından geliştirilmektedir.");
+    }
+}
